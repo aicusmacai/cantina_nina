ec4fb92e72a6f81aab798b067eadcb1acd32c5eb
Rafael <pcrafael0409@gmail.com>
2026-07-04 14:49:37 -0300

    adicionando barra de carregamento global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import NextTopLoader from 'nextjs-toploader';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} bg-pattern-warm text-stone-800 antialiased min-h-screen select-none`}>
+        <NextTopLoader color="#e3a74f" showSpinner={false} height={4} />
         {children}
         <Toaster richColors position="top-right" theme="dark" />
       </body>
